304c90cd17da5973936aab68f4f9b9d0a19656f0
centinela1024tb <centinela1024tb@gmail.com>
2022-01-12 20:38:27 -0600

    Inicialización del proyecto y configuración inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyDZKL7sNgu6tKN4hAWKwHUh7h19WjbBN9E",
+   authDomain: "my-vocabulary-wors.firebaseapp.com",
+   projectId: "my-vocabulary-wors",
+   storageBucket: "my-vocabulary-wors.appspot.com",
+   messagingSenderId: "977001004109",
+   appId: "1:977001004109:web:2aa116b6a9f50accaff004"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
